7a01f31e064b633bdb28713850fca89336cf3daa
nexusautomatadigital-eng <nexusautomata.digital@gmail.com>
2026-05-11 17:19:43 -0500

    Fix selenium github actions

--- multi_scraping.py
+++ multi_scraping.py
@@ -108,10 +108,35 @@ twilio_client = Client(
 # NAVEGADOR
 # ======================================
 
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install())
-)
+# ======================================
+# CHROME OPTIONS GITHUB ACTIONS
+# ======================================
 
+options = webdriver.ChromeOptions()
+
+options.add_argument("--headless")
+
+options.add_argument("--no-sandbox")
+
+options.add_argument("--disable-dev-shm-usage")
+
+options.add_argument("--disable-gpu")
+
+options.add_argument("--window-size=1920,1080")
+
+options.add_argument("--remote-debugging-port=9222")
+
+# ======================================
+# DRIVER
+# ======================================
+
+driver = webdriver.Chrome(
+
+    service=Service(ChromeDriverManager().install()),
+
+    options=options
+
+)
 # ======================================
 # LOOP CLIENTES
 # ======================================
